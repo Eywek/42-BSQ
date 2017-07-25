715107c44ab90a7b0238bf2c18b6d20ac5341ea7
Valentin TOUFFET <vtouffet@e2r3p12.42.fr>
2017-07-26 01:44:39 +0200

    Norme

--- srcs/parser.c
+++ srcs/parser.c
@@ -6,7 +6,7 @@
 /*   By: vtouffet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/24 13:54:40 by vtouffet          #+#    #+#             */
-/*   Updated: 2017/07/25 21:43:37 by vtouffet         ###   ########.fr       */
+/*   Updated: 2017/07/26 01:43:05 by vtouffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int	**handle_file(char *filename)
 	int		k[4];
 	int		**map;
 
-	printf("file: %s\n", filename);
 	k[0] = 0;
 	k[1] = 0;
 	k[3] = 0;
@@ -30,15 +29,13 @@ int	**handle_file(char *filename)
 		fd = 0;
 	if (fd == -1)
 		return (map);
-printf("fd: %d\n", fd);
 	map = read_file(fd, map, 1, k);
 	close(fd);
 	return (map);
 }
 
-int	**save_first_line(t_list **node, int **map, int *first)
+int	**save_first_line(t_list **node, int **map, int *first, int i)
 {
-	int		i;
 	char	*fline;
 	t_list	*current;
 	int		l_count;
@@ -47,14 +44,10 @@ int	**save_first_line(t_list **node, int **map, int *first)
 	if (!(fline = (char*)malloc(sizeof(char) * ft_list_size(*node))))
 		return (NULL);
 	current = *node;
-	i = 0;
 	while (current && (fline[i++] = current->data))
 		current = current->next;
 	fline[i] = '\0';
-	i = 0;
 	l_count = ft_atoi(fline);
-	printf("First line: %s\n", fline);
-	printf("Compare lines (%d != %d + 3)\n", ft_strlen(fline), nb_size(l_count) + 3);
 	ft_list_clear(node);
 	free(current);
 	if ((length = ft_strlen(fline)) != nb_size(l_count) + 3)
@@ -64,10 +57,6 @@ int	**save_first_line(t_list **node, int **map, int *first)
 	if (!(map[0] = (int*)malloc(sizeof(int) * 9)))
 		return (NULL);
 	map = set_config(map, fline, l_count, length);
-	printf("config[0] = %c\n", map[0][0]);
-	printf("config[1] = %c\n", map[0][1]);
-	printf("config[2] = %c\n", map[0][2]);
-	printf("config[4] = %d\n", map[0][4]);
 	free(fline);
 	*first = 1;
 	return (map);
@@ -83,7 +72,7 @@ int	**save_second_line(t_list **node, int **map, int index)
 	if (!(map[index] = (int*)malloc(sizeof(int) * (node_size))))
 		return (NULL);
 	map[0][3] = node_size;
-	printf("config[3] = %d\n", map[0][3]);
+	map[0][5] = 0;
 	i = 0;
 	current = *node;
 	while (current)
@@ -91,7 +80,6 @@ int	**save_second_line(t_list **node, int **map, int index)
 		if (current->data != map[0][0] && current->data != map[0][1])
 			return (NULL);
 		map[index][i++] = (current->data == map[0][0]) ? 1 : 0;
-		printf("map[%d][%d] = %d\n", index, i-1, map[index][i-1]);
 		current = current->next;
 	}
 	i = 0;
@@ -105,7 +93,6 @@ int	**save_lines(int **map, int *index, char c, int *i)
 		return (NULL);
 	if (c == '\n')
 	{
-		printf("-- NEW LINE --\n");
 		++(*index);
 		if (*index > 2 && *i < map[0][3] - 1)
 			return (NULL);
@@ -118,7 +105,6 @@ int	**save_lines(int **map, int *index, char c, int *i)
 		if (c != map[0][0] && c != map[0][1])
 			return (NULL);
 		map[*index][(*i)++] = (c == map[0][0]) ? 1 : 0;
-		printf("map[%d][%d] = %d\n", *index, *i-1, map[*index][*i-1]);
 	}
 	return (map);
 }
@@ -137,19 +123,17 @@ int	**read_file(int fd, int **map, int index, int *k)
 			if (buffer[k[2]] == '\n')
 				k[0]++;
 			if (k[0] == 1 && k[1] == 0)
-				map = save_first_line(&node, map, &k[1]);
+				map = save_first_line(&node, map, &k[1], 0);
 			else if (k[0] == 0 || k[0] == 1)
 				ft_list_push_back(&node, buffer[k[2]]);
 			else if (k[0] == 2 && node)
 				map = save_second_line(&node, map, 1);
-			if (k[0] >= 2  && map != NULL)
+			if (k[0] >= 2 && map != NULL)
 				map = save_lines(map, &index, buffer[k[2]], &k[3]);
 			if (map == NULL && k[0] >= 1)
 				return (NULL);
 			buffer[k[2]] = '\0';
 		}
 	}
-	map[0][8] = --index;
-	printf("config[8] = %d\n", index);
-	return (map);
+	return ((map[0][8] = --index) ? map : map);
 }
--- srcs/resolve.c
+++ srcs/resolve.c
@@ -6,7 +6,7 @@
 /*   By: jechoque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/24 19:16:39 by jechoque          #+#    #+#             */
-/*   Updated: 2017/07/25 23:36:01 by vtouffet         ###   ########.fr       */
+/*   Updated: 2017/07/26 00:54:01 by vtouffet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ int	check_arround(int i, int j, int k)
 	return (k);
 }
 
-int	**stock_coord(int **map, int x, int y)
+int	**stock_coord(int **map, int w, int x, int y)
 {
-	if (map[y][x] >= map[0][5])
+	if (w >= map[0][5])
 	{
-		map[0][5] = map[y][x];
+		map[0][5] = w;
 		map[0][6] = x;
 		map[0][7] = y;
 	}
@@ -39,32 +39,22 @@ int	**resolve(int **map)
 	int x;
 	int y;
 
-	x = 0;
-	y = 0;
-	//printf("%d != %d\n", map[0][4], map[0][3]);
 	if (map == NULL || map[0][4] != map[0][8])
 		return (NULL);
-	map = stock_coord(map, (map[0][3] - 1), map[0][4]);
-	y = (map[0][4] + 1);
-	display(map);
+	x = map[0][3];
+	y = map[0][4] + 1;
+	map = stock_coord(map, map[y][x], (x - 1), y);
 	while (y-- > 1)
 	{
 		x = (map[0][3]);
-		//printf("%d != %d\n", map[0][4], map[0][3]);
 		while (--x >= 0)
-		{
-			printf("%d\n", map[y][x]);
 			if (map[y][x] == 1)
 			{
 				if (x != (map[0][3] - 1) && y != (map[0][4]))
 					map[y][x] = (1 + check_arround(map[y][x + 1], map[y + 1][x],
 							map[y + 1][x + 1]));
-				map = stock_coord(map, x, y);
+				map = stock_coord(map, map[y][x], x, y);
 			}
-			printf("x: %d  y: %d\n", x, y);
-			display(map);
-			write(1, "\n\n", 2);
-		}
 	}
 	return (map);
 }
